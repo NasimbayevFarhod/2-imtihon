819600d05e236c66bd25ca763d0da27d6dc7a833
NasimbayevFarhod <105714502+NasimbayevFarhod@users.noreply.github.com>
2022-07-21 22:05:51 +0500

    new pages

--- js/main.js
+++ js/main.js
@@ -6,9 +6,19 @@ elModalOpenButton.addEventListener("click", function(){
   elModal.classList.add("modal-show");
 })
 
-
 var elModalCloseButton = document.querySelector(".js-close-modal");
 
 elModalCloseButton.addEventListener("click" , function (){
   elModal.classList.remove("modal-show");
 })
+
+
+
+
+// var elInfoOpenButton = document.querySelector(".js-info-open-button");
+// var elClose = document.querySelector(".closed");
+
+
+// elInfoOpenButton.addEventListener("click" ,function(){
+//   elClose.classList.toggle("opened")
+// })
